853bf1219d17cf20c4184db4ebfad00f3d4b5e21
annamat <anna.matveev@gmail.com>
2019-09-06 17:47:55 +0300

    Change ribbon triangle to be in a fixed length

--- app/components/Ribbon/index.js
+++ app/components/Ribbon/index.js
@@ -1,12 +1,18 @@
 import styled from 'styled-components';
 
 const Ribbon = styled.span`
-  clip-path: polygon(0% 0%, 100% 0%, 90% 50%, 100% 100%, 0% 100%);
+  clip-path: polygon(
+    0 0,
+    calc(100% - 8px) 0,
+    100% 50%,
+    calc(100% - 8px) 100%,
+    0 100%
+  );
   background-color: #eaeaea;
-  padding: 4px 10px 4px 4px;
+  padding: 4px 8px 4px 4px;
   font-size: 12px;
-  margin-right: 10px;
   color: grey;
+  margin-right: 8px;
 `;
 
 export default Ribbon;
